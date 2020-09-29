00977591196feefde2d9dc7ce61bdb50fd2ddbf0
Marcos Monteiro <marcosmmb90@gmail.com>
2020-09-29 13:08:35 -0300

    Add utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import mock
 import pytest
-from explorer_api import utils
+from explorer_api.utils import Caller, rpc
 from fastapi import HTTPException
 
 
@@ -9,19 +9,26 @@ class TestCaller:
     def test_call_fail(self, mock_post):
         mock_post.return_value.json.return_value = {"error": "Error message"}
         with pytest.raises(HTTPException):
-            utils.Caller.call(dict())
+            Caller.call(dict())
 
     @mock.patch("explorer_api.utils.Caller._post")
     def test_call(self, mock_post):
         expected_response = {"example": "value"}
         mock_post.return_value.json.return_value = expected_response
-        assert utils.Caller.call(dict()) == expected_response
+        assert Caller.call(dict()) == expected_response
+
+    @mock.patch("httpx.post")
+    def test_post(self, mock_httpx_post):
+        Caller._post(data := {"key": "value"})
+        mock_httpx_post.assert_called_once_with(
+            Caller.uri, data=data, headers=Caller.headers
+        )
 
 
 class TestRPCDecorator:
     @mock.patch("explorer_api.utils.Caller.call")
     def test_rpc(self, mock_call):
-        @utils.rpc
+        @rpc
         def f(arg):
             return arg
 
