4396fa7506c28eed288645f3a24e021b04418f50
Marcos Monteiro <marcosmmb90@gmail.com>
2020-09-29 09:43:48 -0300

    Refactor rpc modules

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -3,23 +3,22 @@ import mock
 from explorer_api import utils
 
 
-def mock_rpc_call(function):
-    @staticmethod
+def mock_rpc(function):
     def wrapper(*args, **kwargs):
         return function(*args, **kwargs)
     return wrapper
 
-mock.patch("explorer_api.utils.rpc_call", side_effect=mock_rpc_call).start()
+mock.patch("explorer_api.utils.rpc", side_effect=mock_rpc).start()
 
-from explorer_api.rpc import RPCNodeClient
+from explorer_api import rpc
 
 
-class TestRPCNodeClient:
+class TestRPC:
     example_address = "nano_3x4ui45q1cw8hydmfdn4ec5ijfdqi4ryp14g4ayh71jcdkwmddrq7ca9xzn9"
     example_hash = "CD0A56F7729EBBF62A81235AF34D1D69362F1FCD2542734BD8FEBD9D2EB6C130"
 
     def test_account_history(self):
-        response = RPCNodeClient.account_history(self.example_address, 5, 2)
+        response = rpc.account_history(self.example_address, 5, 2)
         expected_response = {
             "action": "account_history",
             "account": self.example_address,
@@ -29,7 +28,7 @@ class TestRPCNodeClient:
         assert response == expected_response
 
     def test_account_info(self):
-        response = RPCNodeClient.account_info(self.example_address, True, False, True)
+        response = rpc.account_info(self.example_address, True, False, True)
         expected_response = {
             "action": "account_info",
             "account": self.example_address,
@@ -40,7 +39,7 @@ class TestRPCNodeClient:
         assert response == expected_response
 
     def test_block_info(self):
-        response = RPCNodeClient.block_info(self.example_hash)
+        response = rpc.block_info(self.example_hash)
         expected_response = {
             "action": "block_info",
             "json_block": True,
@@ -49,7 +48,7 @@ class TestRPCNodeClient:
         assert response == expected_response
 
     def test_delegators(self):
-        response = RPCNodeClient.delegators(self.example_address)
+        response = rpc.delegators(self.example_address)
         expected_response = {
             "action": "delegators",
             "account": self.example_address,
@@ -57,7 +56,7 @@ class TestRPCNodeClient:
         assert response == expected_response
 
     def test_pending(self):
-        response = RPCNodeClient.pending(self.example_address, 5, False)
+        response = rpc.pending(self.example_address, 5, False)
         expected_response = {
             "action": "pending",
             "account": self.example_address,
