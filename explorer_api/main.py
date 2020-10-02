5a04866a2c520080485f90eb977aa8b20704bc95
Marcos Monteiro <marcosmmb90@gmail.com>
2020-10-02 13:53:11 -0300

    Add v1 prefix

--- explorer_api/main.py
+++ explorer_api/main.py
@@ -3,11 +3,15 @@ from fastapi import FastAPI
 from . import __version__
 from .routers import accounts, blocks
 
-app = FastAPI(
+
+app = FastAPI(docs_url=False, redoc_url=False)
+
+v1 = FastAPI(
     title="BlockLattice.org Explorer API",
     version=__version__,
 )
 
+v1.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
+v1.include_router(blocks.router, prefix="/blocks", tags=["blocks"])
 
-app.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
-app.include_router(blocks.router, prefix="/blocks", tags=["blocks"])
+app.mount("/v1", v1)
